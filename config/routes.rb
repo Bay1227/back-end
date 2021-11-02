4f3dedf07ba1646958f2afe1011b60d32101aa0e
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-11-02 09:42:53 -0700

    secont push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :notes
+  # namespace :api do 
+  #   namespace :v1 do
+      resources :notes
+  #     post '/login', to: 'auth#create'
+  #     get '/profile', to: 'users#profile'
+  #   end 
+  # end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/cookie_click", to: "sessions#click"
 end
